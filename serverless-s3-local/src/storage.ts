913ca35363323351b44be459cf81e9f47df7be98
Masahiro Wada <argon.argon.argon@gmail.com>
2024-03-24 06:43:35 +0000

    refactor

--- serverless-s3-local/src/storage.ts
+++ serverless-s3-local/src/storage.ts
@@ -6,11 +6,47 @@ import {
   start as startMinio,
 } from "./backend/minio";
 import { Logger, nullLogger } from "./logger";
-import { startServer as startProxyServer } from "./webproxy";
+import { WebsiteConfig, startServer as startProxyServer } from "./webproxy";
+
+const DEFAULT_PROXY_PORT = 2929;
 
 export type StorageConfig = {
   Minio: MinioConfig;
   proxyPort?: number;
+};
+
+export namespace StorageConfig {
+  // biome-ignore lint/suspicious/noExplicitAny: Custom has any type
+  export const of = (config: Record<string, any>): StorageConfig => {
+    return {
+      Minio: MinioConfig.of(config.minio),
+      proxyPort: config.proxyPort,
+    };
+  };
+}
+
+const spawnProxyServer = async (
+  config: StorageConfig,
+  websites: WebsiteConfig[],
+): Promise<{ port: number; close: () => void }> => {
+  const { port, close } = await startProxyServer({
+    Port: config.proxyPort ?? DEFAULT_PROXY_PORT,
+    TargetEndpoint: `http://${config.Minio.address}:${config.Minio.port}`,
+    Websites: websites,
+  });
+
+  return { port, close };
+};
+
+const collectWebsiteConfigs = (
+  configs: Record<string, BucketConfig>,
+): WebsiteConfig[] => {
+  return Object.values(configs).flatMap((config) => {
+    if (config.Website) {
+      return [config.Website];
+    }
+    return [];
+  });
 };
 
 export const start = async (
@@ -20,28 +56,27 @@ export const start = async (
   notificationConfigs: NotificationConfig[],
   log: Logger = nullLogger,
 ): Promise<() => void> => {
-  const close = await startMinio(
+  const closeFuncs: (() => void)[] = [];
+
+  const closeMinio = await startMinio(
     config.Minio,
     bucketConfigs,
     bucketPolicies,
     notificationConfigs,
     log,
   );
+  closeFuncs.push(closeMinio);
 
-  const websites = Object.values(bucketConfigs).flatMap((config) => {
-    if (config.Website) {
-      return [config.Website];
-    }
-    return [];
-  });
-  const { close: closeProxy } = await startProxyServer({
-    Port: config.proxyPort ?? 2929,
-    TargetEndpoint: `http://${config.Minio.address}:${config.Minio.port}`,
-    Websites: websites,
-  });
+  const websites = collectWebsiteConfigs(bucketConfigs);
+  if (0 < websites.length) {
+    const { port, close } = await spawnProxyServer(config, websites);
+    log.info(`Proxy server started at http://localhost:${port}`);
+    closeFuncs.push(close);
+  }
 
   return () => {
-    closeProxy();
-    close();
+    for (const close of closeFuncs) {
+      close();
+    }
   };
 };
